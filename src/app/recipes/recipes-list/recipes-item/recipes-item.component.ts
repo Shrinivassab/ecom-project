216852abdc5d787a3f7489f91428c5147f43f916
shrinivassab <shrinivassab@gmail.com>
2020-04-21 15:42:10 -0400

    - Course project - The Components and Data Bindings

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Recipes} from "../../recipes.model";
 
 @Component({
   selector: 'app-recipes-item',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesItemComponent implements OnInit {
 
+  @Output() recipeSelected = new EventEmitter<void>();
+  @Input() recipe: Recipes;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
